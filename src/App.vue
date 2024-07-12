7f50c9d407d0e6ed6406df076ef57ae55628a46b
Math3usH <matheushenriquecalsavara@gmail.com>
2024-07-12 16:48:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+  <h1>componentes</h1>
+  <ExpandBox />
+  <ExpandBox />
+  <ExpandBox />
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+const showContent = ref(false)
+</script>
+
+<template>
+    <div>
+        <button @click="showContent = !showContent">Mostrar</button>
+        <div v-if="showContent" class="Expand-box">aqui vai um texto escondido</div>
+    </div>
+</template>
+
+<style scoped>
+.Expand-box{
+    height: 100%;
+    padding: 20px;
+    border: 1px solid black;
+    box-shadow: 2px 2px 3px 3px gray;
+    border-radius: 10px;
+}
+</style>
