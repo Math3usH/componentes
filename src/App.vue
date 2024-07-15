9be18353b73c05619d1e2c65583ca8d1247d5bfd
Math3usH <matheushenriquecalsavara@gmail.com>
2024-07-15 15:04:56 -0300

    expand-box

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import ExpandBox from './components/ExpandBox.vue';
+const caixas = [
+  {id: 1, title: "caixa 1", content: "conteudo 1", quantity: 100},
+  {id: 2, title: "caixa 2"},
+  {id: 3, title: "caixa 3", quantity: 100},
+  {id: 4, title: "caixa 4", content: "conteudo 4"}
+]
 </script>
 
 <template>
   <h1>componentes</h1>
-  <ExpandBox />
-  <ExpandBox />
-  <ExpandBox />
+  <expand-box v-for="caixa in caixas" :key="caixas.id" v-bind="caixa" />
+  <!-- <ExpandBox title="Caixa 1" content="conteudo 1" />
+  <ExpandBox title="Caixa 2" content="conteudo 2" />
+  <ExpandBox title="Caixa 3" /> -->
 </template>
 
 <style scoped></style>
--- src/components/ExpandBox.vue
+++ src/components/ExpandBox.vue
@@ -1,18 +1,39 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+
+// const props = defineProps(['title', "content"])
+defineProps({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        default: "Valor padrão do texto da caixa..."
+    },
+    quantity: {
+        type: Number,
+        default: 10
+    }
+})
 
 const showContent = ref(false)
+const textButton = computed(() => (showContent.value ? 'Esconder' : 'Mostrar'))
 </script>
 
 <template>
     <div>
-        <button @click="showContent = !showContent">Mostrar</button>
-        <div v-if="showContent" class="Expand-box">aqui vai um texto escondido</div>
+        <button @click="showContent = !showContent">{{ textButton }}</button>
+        <div v-if="showContent" class="Expand-box">
+            <h1>{{ title }}</h1>
+            <p>{{ content }}</p>
+            <p>Quantidade: {{ quantity }}</p>
+        </div>
     </div>
 </template>
 
 <style scoped>
-.Expand-box{
+.Expand-box {
     height: 100%;
     padding: 20px;
     border: 1px solid black;
